4a52fb87af7390768fa75e8d34e3bead59eac975
basharatali-web <softwaredivision92@gmail.com>
2025-08-15 15:01:43 +0500

    add index.js

---
+++ pages/pages/api/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🚀 Welcome to LatencyPro SaaS</h1>
+      <p>Your ultimate latency monitoring & SaaS platform.</p>
+    </div>
+  );
+}
